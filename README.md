4235d03b5ef716f078a470b14fd4ee959e509964
luizvale <43688554+luizvale@users.noreply.github.com>
2019-03-26 21:16:16 -0300

    Authorization
    Mudanças no README, e no método fbconnect

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Esse repositório contém um script de configuração da base de dados e um script de uma aplicação web flask
 ## A pasta de templates possui todos os arquivos HTML necessários para renderização assim como
-## static possui arquivos CSS de estilização 
+## static possui arquivos CSS de estilização e imagens
  
  - ***finalproject.py*** Script com funcionalidades CRUD da página bem como códigos de autenticação e autorização
 
@@ -22,8 +22,6 @@
  - O nome da base de dados pode ser modificado a qualquer momento dentro do arquivo database_setupas.py, assim que interpretado, todas as funcionalidades da página estarão disponíveis
 
   - *Cada table em database_setupas.py possui um método serialize para representação JSON
-
-   - A versão final do servidor desse projeto foi inicializada na porta 5050, em uma virtual machine, para fins de compatibilidade com outros processos, sinta-se a vontade para fazer a modificação
 
 # Bibliotecas e frameworks
 - **Flask**
--- finalproject.py
+++ finalproject.py
@@ -104,118 +104,6 @@ def fbdisconnect():
     result = h.request(url, 'DELETE')[1]
     return "you have been logged out"
 
-@app.route('/gconnect', methods=['POST'])
-def gconnect():
-    # Validando o estado do token
-    if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    # Obtem o código de autorização
-    code = request.data
-
-    try:
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
-        oauth_flow.redirect_uri = 'postmessage'
-        credentials = oauth_flow.step2_exchange(code)
-    except FlowExchangeError:
-        response = make_response(
-            json.dumps('Failed to upgrade the authorization code.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Checa se o acesso do token é válido
-    access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
-    h = httplib2.Http()
-    result = json.loads(h.request(url, 'GET')[1])
-
-    if result.get('error') is not None:
-        response = make_response(json.dumps(result.get('error')), 500)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    gplus_id = credentials.id_token['sub']
-    if result['user_id'] != gplus_id:
-        response = make_response(
-            json.dumps("Token's user ID doesn't match given user ID."), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    if result['issued_to'] != CLIENT_ID:
-        response = make_response(
-            json.dumps("Token's client ID does not match app's."), 401)
-        print("Token's client ID does not match app's.")
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    stored_access_token = login_session.get('access_token')
-    stored_gplus_id = login_session.get('gplus_id')
-    if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    login_session['access_token'] = credentials.access_token
-    login_session['gplus_id'] = gplus_id
-
-    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-    params = {'access_token': credentials.access_token, 'alt': 'json'}
-    answer = requests.get(userinfo_url, params=params)
-
-    data = answer.json()
-
-    login_session['username'] = data['name']
-    login_session['picture'] = data['picture']
-    login_session['email'] = data['email']
-
-    user_id = getUserID(data["email"])
-    if not user_id:
-        user_id = createUser(login_session)
-    login_session['user_id'] = user_id
-
-    output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-    output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-    flash("you are now logged in as %s" % login_session['username'])
-    print("done!")
-    return output
-
-@app.route('/gdisconnect')
-def gdisconnect():
-        # Disconecta um usuário já conectado
-    access_token = login_session.get('access_token')
-    if access_token is None:
-        response = make_response(
-            json.dumps('Current user not connected.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[0]
-
-    if result['status'] == '200':
-        # Reseta a sessão de usuário.
-        del login_session['access_token']
-        del login_session['gplus_id']
-        del login_session['username']
-        del login_session['email']
-        del login_session['picture']
-
-        response = make_response(json.dumps('Successfully disconnected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    else:
-        # Se por alguma razão o token de acesso for invalido
-        response = make_response(
-        json.dumps('Failed to revoke token for given user.', 400))
-        response.headers['Content-Type'] = 'application/json'
-        return response
 
 def createUser(login_session):
     newUser = User(name=login_session['username'], email=login_session[
@@ -261,8 +149,8 @@ def categoriesJSON():
 def showCategories():
     categories = session.query(Category).all()
     if 'username' not in login_session:
-        return render_template('publicrestaurants.htm', categories = categories)
-    # Página Inicial com os nomes dos Restaurantes
+        return render_template('login.htm', categories = categories)
+    # Página Inicial com os nomes das categorias
     else:
         return render_template('category_all.htm', categories=categories)
 
@@ -276,7 +164,7 @@ def newCategory():
         session.add(addCategory)
         session.commit()
         flash("new Category was created!")
-        # Página para criar um novo Restaurante
+        # Página para criar uma nova categoria
         return redirect(url_for('showCategories'))
     else:
         return render_template('new_category.htm')
@@ -315,7 +203,7 @@ def editCategory(category_id):
 @app.route('/categories/<int:category_id>/menu')
 @app.route('/categories/<int:category_id>/')
 def showMenu(category_id):
-    #Página com o cardápio da Categoria selecionada
+    #Página com os itens da Categoria selecionada
     categories = session.query(Category).filter_by(id=category_id).one()
     creator = getUserInfo(categories.user_id)
     items = session.query(Item).filter_by(category_id=category_id).all()
